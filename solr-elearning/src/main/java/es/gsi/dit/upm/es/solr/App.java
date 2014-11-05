0c6e5fa7803037cb43dd8f1b7093078c7603b68d
Alberto Mardomingo <alberto.mardomingo@gmail.com>
2014-11-05 12:15:01 +0100

    Created solr branch

---
+++ solr-elearning/src/main/java/es/gsi/dit/upm/es/solr/App.java
@@ -0,0 +1,14 @@
+package es.gsi.dit.upm.es.solr;
+
+/**
+ * Main class
+ * Obviuos WIP
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Nothing to see here yet..." );
+    }
+}
---
+++ solr-elearning/src/test/java/es/gsi/dit/upm/es/solr/AppTest.java
@@ -0,0 +1,39 @@
+package es.gsi.dit.upm.es.solr;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test
+ * Again, just a skeleton right now.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
