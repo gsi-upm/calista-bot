8ece93e1fe9232a385be5a008a047c13da046ef0
Alberto Mardomingo <alberto.mardomingo@gmail.com>
2015-04-27 11:17:00 +0200

    Added popup client for the QA

--- QA-client/js/qa.js
+++ QA-client/js/qa.js
@@ -16,6 +16,10 @@ jQuery(document).ready(function($){
     /* Store the url shown */
     var current_url_shown = "" // this is used to avoid continous recharging of the web
     
+    /* Vademecum base url */
+    var vademecum_base = "http://www.dit.upm.es/~pepe/libros/vademecum/index.html?n="
+
+    
     // Creates (or loads from cookie) a random user id for the bot
     var username = "";
     if(document.cookie && document.cookie.indexOf("botUser") != -1) {
@@ -35,6 +39,13 @@ jQuery(document).ready(function($){
     };
     // Set the username in the span    
     $('#username').html(username);
+
+    /** Hide/show effect */
+    $('#bot_chat_window #upper_bar a').click(function(){
+        $('#bot_chat_window').toggleClass('visible');
+        $('#bot_chat_window #screen_wrapper form input[name=question]').val('');
+        $('#bot_chat_window #screen_wrapper form input[name=question]').focus();
+    });
     
     // Autocomplete
     //$('#question').autocomplete({source: autocom_questions});
@@ -44,6 +55,7 @@ jQuery(document).ready(function($){
         // serialize the form
         var form = $(this);
         var form_data = form.serializeArray();
+        var input_field = form.children('input[name=question]');
         if (form[0].value == '') {
             //TODO: Add an alert to say there needs to be data
             return false;
@@ -55,6 +67,9 @@ jQuery(document).ready(function($){
         });
         json_data.username = username;
 
+        $('#screen').append($(constructDialogEntry('me', json_data.question)));
+        input_field.val('');
+        //scrollDisplay();
         //var data = JSON.stringify(json_data);
 
         jQuery.support.cors = true; // :S
@@ -65,30 +80,39 @@ jQuery(document).ready(function($){
             dataType: 'json',
             contentType: 'application/json;charset=UTF-8',
             success: function (data_resp) {
-                $('#iframe-qa').fadeOut('slow');
-                $('#related-container').fadeOut('slow');
                 console.log(data_resp);
                 // Set the url as a response
                 if (data_resp.resource) {
                     if(data_resp.resource != current_url_shown) {
                         // I want the filename from that url:
-                        $('#iframe-qa').attr('src', data_resp.resource);
-                        current_url_shown = data_resp.resource
-                        $('#iframe-qa').fadeIn('slow');
+                        var name_start = data_resp.resource.lastIndexOf('/')+1
+                        var filename = data_resp.resource.substring(name_start)
+                        $('#iframe-qa').attr('src', vademecum_base + filename);
+                        current_url_shown = vademecum_base + filename;
+                        // Add a generic response
+                        $('#screen').append(
+                            $(constructDialogEntry('Dent',
+                                                   "Te muestro la información que tengo sobre "
+                                                  + data_resp.title)));
+                        $('#iframe-qa').show();
                     }
-                    related_list="<ul>";
+                    /*related_list="<ul>";
                     data_resp.links.forEach(function(entry){
                         related_list+='<li>'+entry.title+'</a>';
                     });
                     related_list+='</ul>';
                     $('#related-list').html(related_list);
-                    $('#related-container').fadeIn('slow');
+                    $('#related-container').fadeIn('slow');*/
                 } else {
-                    alert(data_resp.error);
+                    $('#screen').append(
+                    $(constructDialogEntry('Dent',
+                                            data_resp.error)));
+                    
                 }
             },
             error: function(data_resp){
                 console.log("Error connecting to the contoller");
+                return false;
             }
         });
 
@@ -103,6 +127,21 @@ jQuery(document).ready(function($){
         for (var i = length; i > 0; --i) result += charSet[Math.round(Math.random() * (charSet.length - 1))];
         return result;
     }
+    
+        /* This scrolls the display to the bottom, just to show all the messages.
+     * It can be overriden to use a different animation */
+    function scrollDisplay () {
+        // Nice scroll to last message
+        $("#screen").scrollTo('100%', 0);  
+    }
+    
+    /* This construct html formatted dialog entry */
+    function constructDialogEntry (nick, entry) {
+        return '<div class="dia_entry">\
+                  <span class="nick">' + nick + ':</span>\
+                  <span class="text">' + entry + '</span>\
+                </div>'
+    };
 
 });
 
